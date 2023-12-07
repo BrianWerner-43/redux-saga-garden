8395b42ca6c6c9cffe00036d15ccb80b92fb5454
Brian Werner <brianmw199@gmail.com>
2023-12-06 23:41:25 -0600

    Got our plant list to appear on the DOM

--- src/components/PlantList/PlantList.jsx
+++ src/components/PlantList/PlantList.jsx
@@ -5,16 +5,32 @@ import { useDispatch, useSelector} from 'react-redux';
 function PlantList() {
     const dispatch = useDispatch();
 
-    const reduxState = useSelector(store => store);
+    const plantList = useSelector(store => store.plantList);
+
+    
 
     useEffect(() => {
         // dispatch an action to request the plantList from the API
+        getPlants();
     }, []); 
+
+   const getPlants = () => {
+        dispatch({
+            type: 'SAGA/GET_PLANTS'
+        })
+    }
+
+   
 
     return (
         <div>
             <h3>This is the plant list</h3>
-            <pre>{JSON.stringify(reduxState)}</pre>
+            {/* <pre>{JSON.stringify(reduxState)}</pre> */}
+            <ul>
+                {plantList.map((plantItem) => {
+                  return  <li key={plantItem.id}>{plantItem.name}</li>
+                })}
+            </ul>
         </div>
     );
 }
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,28 +1,62 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
+import logger from 'redux-logger';
+import axios from 'axios';
+import { takeLatest, put } from 'redux-saga/effects';
 
 // this startingPlantArray should eventually be removed
-const startingPlantArray = [
-  { id: 1, name: 'Rose' },
-  { id: 2, name: 'Tulip' },
-  { id: 3, name: 'Oak' }
-];
+// const startingPlantArray = [
+//   { id: 1, name: 'Rose' },
+//   { id: 2, name: 'Tulip' },
+//   { id: 3, name: 'Oak' }
+// ];
 
-const plantList = (state = startingPlantArray, action) => {
+function* rootSaga() {
+  yield takeLatest('SAGA/GET_PLANTS', getPlants)
+}
+
+const plantList = (state = [], action) => {
   switch (action.type) {
-    case 'ADD_PLANT':
-      return [ ...state, action.payload ]
+    case 'GET_PLANTS':
+      return action.payload;
     default:
       return state;
   }
 };
+
+function* getPlants() {
+  try {
+    const response = yield axios({
+      method: 'GET',
+      url: '/api/plants'
+    })
+    yield put({
+      type: 'GET_PLANTS',
+      payload: response.data
+    })
+    
+  } catch(error) {
+    console.log('Getting plants from server faild:', error);
+  }
+}
+
+const sagaMiddleware = createSagaMiddleware();
 
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
 // Note that the store is currently not
 // configured to utilize redux-saga OR
 // redux logger!
 const store = createStore(
-  combineReducers({ plantList }),
+  combineReducers({
+    plantList
+   }),
+   applyMiddleware(sagaMiddleware, logger),
 );
 // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
+
+
+
+sagaMiddleware.run(rootSaga);
+
 
 export default store;
